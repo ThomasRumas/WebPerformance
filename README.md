94d646d4f9832003dd40cca565ca448549138a49
Thomas Rumas <thomas.rumas@ext.leroymerlin.fr>
2018-09-17 18:00:43 +0200

    add possibility to communicate with webpagetest instance from sitespeed.io

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ The script will ask you about parameters :
  - *browser* : On which browser do you want to initalize the test (default chrome) 
  - *iterations* : The number of iterations do you want to test the same url 
  - *bandwidth* : Only on Linux and Mac OS X operating system, you can choose between theses availables connectivities `3g, 3gfast, 3gslow, 2g, cable`
+ - *script* : Like explain on Webpagetest documentation, you can write script test to perform on pages to complete test [documentation](https://sites.google.com/a/webpagetest.org/docs/using-webpagetest/scripting)
 
 If you want to have more connectivities, be free to modify the network object in `index.js` file
 
--- index.js
+++ index.js
@@ -4,11 +4,14 @@
 const sitespeed = require('sitespeed.io/lib/sitespeed');
 const throttle = require('@sitespeed.io/throttle');
 
+//Webpagetest API default value
+var urlWebPageTest = "http://localhost:4000"; 
+
 //Prompt ressources to ask users parameters 
 const prompt = require('prompt'); 
 
 const resultFolder = "/result-" + Date.now(); 
-const OS = process.platform; 
+const OS = process.platform;
 
 const network = {
     '3g' : {
@@ -38,25 +41,21 @@ const network = {
     }
 }
 
-var prmUrl = ""; 
-var prmBrowser = "chrome";
-var prmIterations = 3; 
-
 prompt.start(); 
 
 //Because traffic control doesn't work on win32 system
 if(OS === "win32") {
-    prompt.get(['url', 'browser', 'iterations'], (err, result) => {     
-        sitespeed.run(generateOptions(result)).then((result) => {
+    prompt.get(['url', 'browser', 'iterations', 'webpagetest', 'script', 'useragent'], (err, result) => {     
+        sitespeed.run(generateOptions(result)).then((resultTest) => {
             console.log(`The test is done, go to ${__dirname}${resultFolder}/index.html to see results`); 
         });
     }); 
 } else {
-    prompt.get(['url', 'browser', 'iterations', 'bandwidth'], (err, result) => {
+    prompt.get(['url', 'browser', 'iterations', 'bandwidth', 'webpagetest', 'script', 'useragent'], (err, result) => {
         var bandwidth = result.bandwidth == "" ? "cable" : result.bandwidth; 
 
         throttle.start({up:network[bandwidth].up, down:network[bandwidth].down, rtt:network[bandwidth].rtt}).then(() => {
-            sitespeed.run(generateOptions(result)).then((result) => {
+            sitespeed.run(generateOptions(result)).then((resultTest) => {
                 console.log(`The test is done, go to ${__dirname}${resultFolder}/index.html to see results`); 
             });
         })
@@ -67,17 +66,49 @@ if(OS === "win32") {
 }
 
 function generateOptions(result) {
-    prmUrl = result.url; 
-    prmBrowser = result.browser == "" ? "chrome" : result.browser; 
-    prmIterations = result.iterations == "" ? 3 : parseInt(result.iterations); 
+    /*
+    https://github.com/marcelduran/webpagetest-api
+    --webpagetest.host          The domain of your WebPageTest instance.
+    --webpagetest.key           The API key for your WebPageTest instance. Not needed if private instance
+    --webpagetest.location      The location for the test
+    --webpagetest.connectivity  The connectivity for the test.
+    --webpagetest.runs          The number of runs per URL.
+    --webpagetest.custom        Execute arbitrary Javascript at the end of a test to collect custom metrics.
+    --webpagetest.script        Direct WebPageTest script as a string
+    --webpagetest.file          Path to a script file
+    --webpagetest.useragent     User-agent to declare to go on the webpage, only working on Chrome
+    */
+    let prmUrl = result.url; 
+    let prmBrowser = result.browser == "" ? "chrome" : result.browser; 
+    let prmIterations = result.iterations == "" ? 3 : parseInt(result.iterations); 
+    let prmFile = result.file == "" ? "" : result.file; 
+    //LAN because of Windows and Mac OS X, name we give on our builded docker image
+    let prmConnectivity = result.bandwidth == "" || result.bandwidth === undefined ? "LAN" : result.bandwidth;  
+    let prmUserAgent = result.useragent == "" || result.useragent === undefined ? "" : result.useragent; 
+    urlWebPageTest = result.webpagetest; 
 
     return ({
         urls: [prmUrl], 
         outputFolder: __dirname + resultFolder,
         browsertime: {
             browser: prmBrowser,
-            connectivity: 'native',
-            iterations: prmIterations,
+            connectivity: prmConnectivity,
+            iterations: prmIterations, 
+            userAgent: prmUserAgent
+        }, 
+        webpagetest: {
+            host: urlWebPageTest,
+            location: "Test",
+            pollResults: 10,
+            timeout: 600,
+            includeRepeatView: false,
+            private: false,
+            aftRenderingTime: true,
+            connectivity: prmConnectivity,
+            video: true, 
+            run: prmIterations, 
+            useragent: prmUserAgent, 
+            file: prmFile
         }
     });
 }
